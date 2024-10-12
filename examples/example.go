26b4e52f6523770e75874e8c8b3edddfe0fa428e
NoneBack <chenlan2333@gmail.com>
2024-10-12 17:27:03 +0800

    doc: udpate example

--- examples/example.go
+++ examples/example.go
@@ -1,27 +1,3 @@
-# Go-Taskflow
-A static DAG (Directed Acyclic Graph) task computing framework for Go, inspired by taskflow-cpp, with Go's native capabilities and simplicity, suitable for complex dependency management in concurrent tasks.
-
-## Feature
-- **High extensibility**: Easily extend the framework to adapt to various specific use cases.
-
-- **Native Go's concurrency model**: Leverages Go's goroutines to manage concurrent task execution effectively.
-
-- **User-friendly programming interface**: Simplify complex task dependency management using Go.
-
-- **Static and subflow tasking**: Define static tasks as well as nested subflows for greater modularity.
-
-- **Built-in visualization & profiling tools**: Generate visual representations of tasks and profile task execution performance using integrated tools, making debugging and optimization easier.
-
-## Use Cases
-
-- **Data Pipeline**: Orchestrate data processing stages that have complex dependencies.
-
-- **Workflow Automation**: Define and run automation workflows where tasks have a clear sequence and dependency structure.
-
-- **Parallel Tasking**: Execute independent tasks concurrently to fully utilize CPU resources.
-
-## Example
-```go
 package main
 
 import (
@@ -112,28 +88,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-```
-### How to use visualize taskflow
-```go
-if err := gotaskflow.Visualizer.Visualize(tf, os.Stdout); err != nil {
-		log.Fatal(err)
-}
-```
-`Visualize` generate raw string in dot format, just use dot to draw a DAG svg.
-
-![dot](https://raw.githubusercontent.com/noneback/images/ae31f3ea57f3f1b8d4cf94300a5ff502b2340214/graphviz.svg)
-### How to use profile taskflow
-```go
-if err :=exector.Profile(os.Stdout);err != nil {
-		log.Fatal(err)
-}
-```
-
-`Profile` alse generate raw string in flamegraph format, just use flamegraph to draw a flamegraph svg.
-
-![flg](https://raw.githubusercontent.com/noneback/images/ae31f3ea57f3f1b8d4cf94300a5ff502b2340214/t.svg)
-## What's next
-- [ ] Taskflow Composition
-- [ ] Conditional Tasking
-- [ ] Taskflow Loop Repeatition
