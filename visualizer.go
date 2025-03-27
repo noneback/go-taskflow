c4b850f9cdd9a05548f34059c502ad2d3a7193dc
noneback <chenlan2333@gmail.com>
2025-03-27 14:01:28 +0800

    style(visualizer): restyle code

--- visualizer.go
+++ visualizer.go
@@ -1,11 +1,23 @@
-//go:build !without_visualizer
-
 package gotaskflow
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
 type Visualizer interface {
+	// Visualize generate raw dag text in dot format and write to writer
 	Visualize(tf *TaskFlow, writer io.Writer) error
 }
 
 var vizer = Visualizer(mockVizer{})
+
+var ErrVisualizerNotSupport = errors.New("visualization not support")
+
+type mockVizer struct {
+}
+
+// Visualize implements Visualizer.
+func (v mockVizer) Visualize(tf *TaskFlow, writer io.Writer) error {
+	return ErrVisualizerNotSupport
+}
