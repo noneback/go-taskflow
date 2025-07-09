f8e1652a5f487109b7d9eb950e462b5e886f02c6
NoneBack <chenlan2333@gmail.com>
2025-07-09 11:20:21 +0800

    fix(benchmark): fix concurency config
    Signed-off-by: NoneBack <chenlan2333@gmail.com>

--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -7,7 +7,7 @@ import (
 	gotaskflow "github.com/noneback/go-taskflow"
 )
 
-var executor = gotaskflow.NewExecutor(1)
+var executor = gotaskflow.NewExecutor(6400)
 
 func BenchmarkC32(b *testing.B) {
 	tf := gotaskflow.NewTaskFlow("G")
